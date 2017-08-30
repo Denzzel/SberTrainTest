078d1d6777aca3ea2d39aa50a73b9dba1689d094
denzzel <denissabanov@mail.ru>
2017-08-30 07:45:50 +0300

    added testng.xml and suite, fixed code of tests

--- SberbankTest/src/test/java/ru/sberbank/SberbankTest.java
+++ SberbankTest/src/test/java/ru/sberbank/SberbankTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SberbankTest {
@@ -26,9 +27,13 @@ public class SberbankTest {
         driver.get("https://sberbank.ru/");
         driver.findElement(By.cssSelector(".region-list__name")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Москва')]")).click();
-        String currentLocation = driver.findElement(By.cssSelector(".region-list__name")).getText();
+        try {
+            String currentLocation = driver.findElement(By.cssSelector(".region-list__name")).getText();
+            Assert.assertEquals(currentLocation, "Москва");
+        } catch (Exception e) {
+            Assert.fail("gotoSberbankPageAndChangeLocationToMoscow: Cant load element on page",e);
+        }
 
-        Assert.assertEquals(currentLocation, "Москва");
     }
 
     @Test
@@ -36,75 +41,36 @@ public class SberbankTest {
         driver.get("https://sberbank.ru/");
         driver.findElement(By.cssSelector(".region-list__arrow")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Нижегородская область')]")).click();
-        String currentLocation = driver.findElement(By.cssSelector(".region-list__name")).getText();
-
-        Assert.assertNotEquals(currentLocation,"Москва");
+        try {
+            String currentLocation = driver.findElement(By.cssSelector(".region-list__name")).getText();
+            Assert.assertNotEquals(currentLocation, "Москва");
+        } catch (Exception e) {
+            Assert.fail("gotoSberbankPageAndChangeLocationToNizhniyNovgorodRegion: Cant load element on page",e);
+        }
     }
 
     @Test
-    public void gotoSocialGroupsInFooterCheckLinkToVK() {
+    public void gotoSocialGroupsInFooterCheckSocialLinks() {
         driver.get("https://sberbank.ru/");
         WebElement element = driver.findElement(By.cssSelector(".social__list"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_vk")).click();
-        String linkToVK = driver.getCurrentUrl();
+        String[] linksToSocialGroups = {
+                "http://vk.com/sberbank",
+                "https://www.facebook.com/bankdruzey",
+                "http://twitter.com/sberbank/",
+                "http://www.youtube.com/sberbank",
+                "http://instagram.com/sberbank",
+                "https://ok.ru/sberbank"};
 
-        Assert.assertEquals(linkToVK, "https://vk.com/sberbank");
-    }
+        for(int i = 0; i < linksToSocialGroups.length; i++) {
+            try {
+                driver.findElement(By.xpath(".//a[@href = \"" + linksToSocialGroups[i] + "\"]"));
+            } catch (Exception e) {
+                Assert.fail("Link to " + linksToSocialGroups[i] + "is absent");
+            }
+        }
+        Assert.assertEquals(true,true);
 
-    @Test
-    public void gotoSocialGroupsInFooterCheckLinkToFacebook() {
-        driver.get("https://sberbank.ru/");
-        WebElement element = driver.findElement(By.cssSelector(".social__list"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_fb")).click();
-        String linkToFacebook = driver.getCurrentUrl();
-
-        Assert.assertEquals(linkToFacebook, "https://www.facebook.com/sberbank");
-    }
-
-    @Test
-    public void gotoSocialGroupsInFooterCheckLinkToTwitter() {
-        driver.get("https://sberbank.ru/");
-        WebElement element = driver.findElement(By.cssSelector(".social__list"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_tw")).click();
-        String linkToTwitter = driver.getCurrentUrl();
-
-        Assert.assertEquals(linkToTwitter, "https://twitter.com/sberbank/");
-    }
-
-    @Test
-    public void gotoSocialGroupsInFooterCheckLinkToYouTube() {
-        driver.get("https://sberbank.ru/");
-        WebElement element = driver.findElement(By.cssSelector(".social__list"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_yt")).click();
-        String linkToYouTube = driver.getCurrentUrl();
-
-        Assert.assertEquals(linkToYouTube, "https://www.youtube.com/user/Sberbank");
-    }
-
-    @Test
-    public void gotoSocialGroupsInFooterCheckLinkToInstagram() {
-        driver.get("https://sberbank.ru/");
-        WebElement element = driver.findElement(By.cssSelector(".social__list"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_ins")).click();
-        String linkToInstagram = driver.getCurrentUrl();
-
-        Assert.assertEquals(linkToInstagram, "https://www.instagram.com/sberbank/");
-    }
-
-    @Test
-    public void gotoSocialGroupsInFooterCheckLinkToOK() {
-        driver.get("https://sberbank.ru/");
-        WebElement element = driver.findElement(By.cssSelector(".social__list"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.cssSelector(".social__icon_type_ok")).click();
-        String linkToOK = driver.getCurrentUrl();
-
-        Assert.assertEquals(linkToOK, "https://ok.ru/sberbank");
     }
 
     @AfterClass
